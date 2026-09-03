bc5919272a4b100fe72b7f411e822cd1d2415a9e
FJavier333 <blacfjba3@gmail.com>
2026-09-02 22:46:05 -0600

    fix: restore original footer in main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header/Header";
 import TopStrip from "../components/TopStrip/TopStrip";
 import Footer from "../components/Footer/Footer";
-import FooterNew from "../components/FooterNew/FooterNew";
 import { Outlet, useLocation } from "react-router-dom";
 
 export default function MainLayout() {
